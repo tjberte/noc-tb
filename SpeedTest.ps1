2e956ca08e975c024a8df72bb0dc5339ab7ed68a
tjberte <tjberte@gmail.com>
2022-09-16 11:26:58 -0500

    Update SpeedTest.ps1

--- SpeedTest.ps1
+++ SpeedTest.ps1
@@ -1,4 +1,4 @@
-wget -outfile c:\windows\temp\speedtest.zip -uri https://install.speedtest.net/app/cli/ookla-speedtest-1.2.0-win64.zip
-Expand-Archive -LiteralPath c:\windows\temp\speedtest.zip
-cd c:\windows\temp\speedtest
+wget -outfile c:\temp\speedtest.zip -uri https://install.speedtest.net/app/cli/ookla-speedtest-1.2.0-win64.zip
+Expand-Archive -LiteralPath c:\temp\speedtest.zip -DestinationPath C:\temp\speedtest
+cd c:\temp\speedtest
 .\speedtest.exe --accept-license
